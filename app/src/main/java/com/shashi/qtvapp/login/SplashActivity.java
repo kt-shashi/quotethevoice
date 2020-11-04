fac637f362cc531a0b08236a1f5d22ba85331725
shashiofficial <shashiofficial10@gmail.com>
2020-11-04 13:01:36 +0530

    [Added] Animations in splash activity

--- app/src/main/java/com/shashi/qtvapp/login/SplashActivity.java
+++ app/src/main/java/com/shashi/qtvapp/login/SplashActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.shashi.qtvapp.MainActivity;
 import com.shashi.qtvapp.R;
@@ -14,12 +19,25 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
         Handler handler = new Handler();
         handler.postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, MainActivity.class));
             finish();
-        }, 1000);
+        }, 2400);
+
+        TextView textView = findViewById(R.id.textView_splash_activity);
+        ImageView imageView = findViewById(R.id.imageViewSplash);
+
+        Animation animationImageView = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in_imageview);
+        Animation animationTextView = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in_textview);
+
+        imageView.setAnimation(animationImageView);
+        textView.setAnimation(animationTextView);
+
     }
+
 }

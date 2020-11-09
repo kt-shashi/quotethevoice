09a824c850928ec437a20aeca0242649e4d8d402
shashiofficial <shashiofficial10@gmail.com>
2020-11-09 19:10:15 +0530

    [Added] AboutFragment

---
+++ README.md
@@ -0,0 +1,60 @@
+<div align="center">
+        
+# Freelancing Project | QuoteTheVoice
+
+</div>
+
+<div align="left">
+
+## Features⚡️
+
+* In-app youtube videos
+* Get notified every time a new video is out
+* Collection of all social media handles
+
+<br>
+<hr>
+<br>
+
+</div>
+<div align="center">
+
+## Download apk
+<br>
+
+[Click here](https://github.com/shashi-kant10/quotethevoice/raw/master/apk/QTV.apk)
+
+<br>
+<hr>
+<br>
+
+</div>
+<div align="left">
+
+## Screenshots
+<br>
+
+<table>
+    <tr>
+        <td><img src = "/screenshots/screenshot1.png" ></td>
+        <td><img src = "/screenshots/screenshot2.png" ></td>
+        <td><img src = "/screenshots/screenshot3.png" ></td>
+    </tr>
+    <tr>
+        <td><img src = "/screenshots/screenshot4.png" ></td>
+    </tr>
+</table>    
+
+<br>
+<hr>
+<br>
+
+## Libraries used and their documentation
+
+- [**ImageSlideshow**](https://github.com/denzcoskun/ImageSlideshow)
+
+- [**Glide**](https://github.com/bumptech/glide)
+
+- [**Cloud Firestore**](https://firebase.google.com/docs/firestore/quickstart)
+
+- [**Firebase Cloud Messaging**](https://firebase.google.com/docs/cloud-messaging)
--- app/src/main/java/com/shashi/qtvapp/ui/others/AboutFragment.java
+++ app/src/main/java/com/shashi/qtvapp/ui/others/AboutFragment.java
@@ -1,10 +1,14 @@
 package com.shashi.qtvapp.ui.others;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,7 +16,7 @@ import androidx.fragment.app.Fragment;
 
 import com.shashi.qtvapp.R;
 
-public class AboutFragment extends Fragment {
+public class AboutFragment extends Fragment implements View.OnClickListener {
     public AboutFragment() {
 
     }
@@ -21,8 +25,20 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about, container, false);
-        TextView textView = view.findViewById(R.id.text_about);
-        textView.setText("About");
+
+        Button button = view.findViewById(R.id.button_mail);
+        button.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:" + "srianshsankhi@gmail.com"));
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(getActivity(), "Could not open mail", Toast.LENGTH_SHORT).show();
+        }
     }
 }
